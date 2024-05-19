4e43e95b37dc9955981a94f964ae3d0f0e6375af
Nsralla <nsrhassan22@gmail.com>
2024-05-19 14:19:30 +0300

    Improve errors log

--- app/src/main/java/Activities/SigninActivity.java
+++ app/src/main/java/Activities/SigninActivity.java
@@ -11,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import Hashing.Hash;
@@ -32,6 +33,13 @@ public class SigninActivity extends AppCompatActivity {
     EditText confirmPasswordText;
     EditText phoneText;
 
+    TextView emailR;
+    TextView phoneR;
+    TextView passR;
+    TextView confR;
+    TextView firstR;
+    TextView lastR;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +56,12 @@ public class SigninActivity extends AppCompatActivity {
         passwordText = findViewById(R.id.password_l2);
         confirmPasswordText = findViewById(R.id.confirmPasswordEditText);
         phoneText = findViewById(R.id.phoneEditText);
+        emailR = findViewById(R.id.emailR);
+        phoneR = findViewById(R.id.phoneR);
+        firstR = findViewById(R.id.firstR);
+        lastR = findViewById(R.id.lastR);
+        passR = findViewById(R.id.passR);
+        confR = findViewById(R.id.confR);
         signinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,40 +91,67 @@ public class SigninActivity extends AppCompatActivity {
         for(int i = 0; i< emails.size(); i++){
             if(emails.get(i).equals(email)){
                 errors.append("Email already in use.\n");
+                emailR.setText("Email already in use.\n");
                 isValid = false;
             }
         }
+        if(isValid)
+            emailR.setText("");
 
         //TODO: Validate the email
         if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             errors.append("Invalid email format.\n");
+            emailR.setText("Invalid email format.\n");
             isValid = false;
+        }
+        else if(isValid){
+            emailR.setText("");
         }
 
         //TODO:VALIDATE THE PHONE NUMBER
         if(!phoneNumber.matches("05\\d{8}"))
         {
             errors.append("Phone number must start with '05' and be 10 digits long.\n");
+            phoneR.setText("Phone number must start with '05' and be 10 digits long.\n");
             isValid = false;
         }
+        else{
+            phoneR.setText("");
+        }
+
 
         //TODO:VALIDATE THE FIRST NAME
         if (firstName.length() < 3 || lastName.length() < 3) {
             errors.append("First name and Last name must be at least 3 characters long.\n");
+            firstR.setText("First name and Last name must be at least 3 characters long.\n");
             isValid = false;
         }
+        else{
+            firstR.setText("");
+        }
+
 
         //TODO: VALIDATE THE PASSWORD
         if (password.length() < 8 || !password.matches(".*[a-zA-Z].*") || !password.matches(".*\\d.*")) {
             errors.append("Password must be at least 8 characters long and include at least one letter and one number.\n");
+            passR.setText("Password must be at least 8 characters long and include at least one letter and one number.\n");
             isValid = false;
         }
+        else{
+            passR.setText("");
+        }
+
 
         //TODO: VALIDATE CONFIRM PASSWORD
         if (!confirmPassword.equals(password)) {
             errors.append("Confirm password does not match the password.\n");
+            confR.setText("Confirm password does not match the password.\n");
             isValid = false;
         }
+        else{
+            confR.setText("");
+        }
+
 
         if(!isValid){
             Toast.makeText(this, errors.toString(), Toast.LENGTH_LONG).show();
